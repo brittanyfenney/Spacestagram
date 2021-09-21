2ac31c0babc0867f4d4969ea4eb60568d5aae600
Brittany Fenney <brittanyfenney@gmail.com>
2021-09-20 18:23:46 -0700

    fetch 10 photos from API

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -2,8 +2,37 @@ import React, { useState, useEffect } from "react";
 import { Photo } from "./Photo";
 
 export const Scroll = () => {
+  const [photos, setPhotos] = useState([]);
+
+  const apiKey = "DEMO_KEY"
+
+  const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=10`;
+
+  async function getPhotos() {
+    console.log('in getPhotos')
+    try {
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+      setPhotos([...photos, ...data]);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    console.log('in useEffect getPhotos')
+    getPhotos();
+  }, []);
 
   return (
-    <p>Hi!</p>
+    <div className="image-container">
+        {photos && photos.length
+          ? photos.map((photo) => {
+              return (
+                <Photo photo={photo} key={photo.url}/>
+              );
+            })
+          : "Looking for photos"}
+      </div>
 );
 };
