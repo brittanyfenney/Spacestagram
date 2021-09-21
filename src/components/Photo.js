1dcdc612c8a92a2841d1d99cb4a3e55c10383691
Brittany Fenney <brittanyfenney@gmail.com>
2021-09-20 18:25:28 -0700

    display photos

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 
 export const Photo = (props) => {
-
+  const photo = props.photo;
 
   return (
-    <p>Photo goes here!</p>
+    <div className="image" key={photo.url}>
+      <img src={photo.url} alt={photo.explanation} />
+    </div>
   );
 };
